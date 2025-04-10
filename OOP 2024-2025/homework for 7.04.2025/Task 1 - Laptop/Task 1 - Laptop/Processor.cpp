b9d62b3555069118c004d048116dbac02f76b477
Nick <nikolay.neykov178@gmail.com>
2025-04-10 13:29:57 +0300

    class Processor - done (beta)

--- OOP 2024-2025/homework for 7.04.2025/Task 1 - Laptop/Task 1 - Laptop/Processor.cpp
+++ OOP 2024-2025/homework for 7.04.2025/Task 1 - Laptop/Task 1 - Laptop/Processor.cpp
@@ -1,11 +1,61 @@
 #include "Processor.hpp"
+#include <iostream>
+#include <exception>
 
-Processor::Processor() {
-	this->frequency = 3;
-	this->numberOfCores = 4;
-	this->cash = 10;
+Processor::Processor(double frequency, unsigned int numberOfCores, unsigned cash) {
+	if (frequency <= 0) {
+		throw std::invalid_argument("Invalid frequency param!");
+	}
+	if (numberOfCores == 0) {
+		throw std::invalid_argument("Invalid number of cores param!");
+	}
+	if (cash <= 0) {
+		throw std::invalid_argument("Invalid cash param!");
+	}
+
+	this->frequency = frequency;
+	this->numberOfCores = numberOfCores;
+	this->cash = cash;
 }
 
-Processor::Processor(double frequency, unsigned int numberOfCores, double cash) {
+double Processor::GetFrequency() const {
+	return this->frequency;
+}
 
+unsigned int Processor::GetNumberOfCores() const {
+	return this->numberOfCores;
+}
+
+unsigned int Processor::GetCash() const {
+	return this->cash;
+}
+
+void Processor::SetFrequency(const double frequency) {
+	if (frequency <= 0) {
+		std::cout << "Invalid frequency!" << std::endl;
+		return;
+	}
+	else {
+		this->frequency = frequency;
+	}
+}
+
+void Processor::SetNumberOfCores(const unsigned n) {
+	if (n == 0) {
+		std::cout << "Invalid number of cores!" << std::endl;
+		return;
+	}
+	else {
+		this->numberOfCores = n;
+	}
+}
+
+void Processor::SetCash(const unsigned cash) {
+	if (cash <= 0) {
+		std::cout << "Invalid cash param!" << std::endl;
+		return;
+	}
+	else {
+		this->cash = cash;
+	}
 }
--- OOP 2024-2025/homework for 7.04.2025/Task 1 - Laptop/Task 1 - Laptop/Processor.hpp
+++ OOP 2024-2025/homework for 7.04.2025/Task 1 - Laptop/Task 1 - Laptop/Processor.hpp
@@ -2,19 +2,19 @@
 
 class Processor {
 public:
-	Processor();
-	Processor(double frequency, unsigned int numberOfCores, double cash);
+	Processor() = delete;
+	Processor(double frequency, unsigned int numberOfCores, unsigned int cash);
 
 	double GetFrequency() const;
 	unsigned int GetNumberOfCores() const;
-	double GetCash() const;
+	unsigned int GetCash() const;
 
 	void SetFrequency(const double frequency);
 	void SetNumberOfCores(const unsigned n);
-	void SetCash(double cash);
+	void SetCash(const unsigned cash);
 
 private:
 	double frequency; //in GHz
 	unsigned int numberOfCores;
-	double cash;
+	unsigned int cash;
 };
